70f32b8130a2a91f7aade66d0b2ef7614997115c
ujjwal <jhaujjwal13@gmail.com>
2020-10-07 15:40:23 +0530

    first commit

---
+++ ppmp-tool/src/main/java/org/ujjhappmp/com/ppmptool/PpmpToolApplication.java
@@ -0,0 +1,13 @@
+package org.ujjhappmp.com.ppmptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmpToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmpToolApplication.class, args);
+	}
+
+}
---
+++ ppmp-tool/src/test/java/org/ujjhappmp/com/ppmptool/PpmpToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ujjhappmp.com.ppmptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmpToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
